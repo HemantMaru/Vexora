dcdd9cff5f69d410e6500b52681eacdca7d7b656
Hemant Maru <hemantkumawat399@gmail.com>
2026-04-30 22:13:45 +0530

    ready for render deploy

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,8 @@
+import "dotenv/config";
 import app from "./src/app.js";
+const PORT = process.env.PORT || 3000;
 import { MongooseConnect } from "./src/config/database.js";
 MongooseConnect();
-app.listen(3000, () => {
-  console.log("server is running on port 3000");
+app.listen(PORT, () => {
+  console.log("server is running on port" + PORT);
 });
